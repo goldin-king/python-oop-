e8553e3d2ca09b3791cf6d8291f46e20e27375f0
thereciandanu@gmail.com <thereciandanu@gmail.com>
2025-04-16 16:43:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+
+buddy = Pet("Buddy")
+
+
+buddy.get_status()
+buddy.eat()
+buddy.sleep()
+buddy.play()
+buddy.train("roll over")
+buddy.train("sit")
+buddy.show_tricks()
+buddy.get_status()
