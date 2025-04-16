e2456337c18aafdc2ef5da73a82813f792d013fa
Abdulmuj33b <121964859+Abdulmuj33b@users.noreply.github.com>
2025-04-16 21:49:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from pet import Pet
+
+# Create a pet object
+max_pet = Pet("Max")
+
+# Test pet methods
+max_pet.eat()
+max_pet.play()
+max_pet.sleep()
+max_pet.train("roll over")
+max_pet.train("play dead")
+
+# Show tricks and status
+max_pet.show_tricks()
+max_pet.get_status()
